a99e53dc124afc6489cb0e6e2257bb9dfd652a14
dhav211 <dhav211@gmail.com>
2023-11-13 18:49:07 -0800

    Add searchbar

--- src/index.js
+++ src/index.js
@@ -12,50 +12,13 @@ async function getForecast(cityName) {
   return weather;
 }
 
-const cityForm = document.getElementById("city-form");
-const submitForm = document.getElementById("submit-form");
-cityForm.value = "";
-
-submitForm.addEventListener("click", async (e) => {
-  e.preventDefault();
-  console.log("submit form");
-  if (cityForm.value.length > 0) {
-    const name = await findCity(cityForm.value);
-
-    if (name.length > 0) {
-      setFields(name);
-      cityForm.value = "";
-    }
-  }
-});
-
-// From an attempt of the city name, this will search it and return the first value
-async function findCity(cityNameAttempt) {
-  const response = await fetch(
-    `https://api.weatherapi.com/v1/search.json?key=a153e9f2c28c497e9e613836230111&q=${cityNameAttempt}`,
-    { mode: "cors" },
-  );
-  const cities = await response.json();
-
-  return cities[0].name;
-}
-
-async function setFields(cityName) {
+export async function setContent(cityName) {
   const forecast = await getForecast(cityName);
-  console.log(forecast);
   const cityHeader = document.getElementById("city-header");
   document.getElementById("current-time-date").textContent =
     setCurrentTime(forecast);
-  // document
-  //   .getElementById("daily-weather-holder")
-  //   .appendChild(createDailyWeather(forecast["forecast"]));
 
   cityHeader.textContent = forecast["location"].name;
-  // currentTemp.textContent = `Current Temperature: ${forecast["current"].temp_f}F`;
-
-  // document
-  //   .getElementById("hourly-weather-holder")
-  //   .appendChild(createHourlyForecast(forecast["forecast"]));
 
   const currentWeatherDetails = document.getElementById(
     "current-weather-details-container",
@@ -112,4 +75,4 @@ function setCurrentTime(forecast) {
   });
 }
 
-setFields("sioux falls");
+setContent("sioux falls");
---
+++ src/search-bar.js
@@ -0,0 +1,101 @@
+import { setContent } from "./index.js";
+
+const cityForm = document.getElementById("city-form");
+const submitForm = document.getElementById("submit-form");
+cityForm.value = "";
+let isSuggestionBoxOpen = false;
+
+submitForm.addEventListener("click", async (e) => {
+  e.preventDefault();
+  if (cityForm.value.length > 0) {
+    const cities = await findCities(
+      cityForm.value.length > 0 ? cityForm.value : "d",
+    );
+
+    if (cities.length > 0) {
+      setCity(cities[0].url);
+    }
+  }
+});
+
+cityForm.addEventListener("input", async () => {
+  const cities = await findCities(
+    cityForm.value.length > 0 ? cityForm.value : "d",
+  );
+
+  if (!isSuggestionBoxOpen && cities.length > 0) {
+    isSuggestionBoxOpen = true;
+
+    const suggestionBox = createSuggestionBox();
+
+    const cityList = document.createElement("ul");
+    suggestionBox.appendChild(cityList);
+
+    fillCityList(cityList, cities);
+    document.getElementById("city-search-form").appendChild(suggestionBox);
+  } else if (isSuggestionBoxOpen && cities.length > 0) {
+    const cityList = document
+      .getElementById("suggestion-box")
+      .querySelector("ul");
+    cityList.replaceChildren();
+    fillCityList(cityList, cities);
+  } else if (isSuggestionBoxOpen && cities.length === 0) {
+    closeSuggestionBox();
+  }
+});
+
+cityForm.addEventListener("focusout", () => {
+  setTimeout(closeSuggestionBox, 200);
+});
+
+cityForm.addEventListener("focusin", () => {});
+
+// From an attempt of the city name, this will search it and return the first value
+async function findCities(cityNameAttempt) {
+  const response = await fetch(
+    `https://api.weatherapi.com/v1/search.json?key=a153e9f2c28c497e9e613836230111&q=${cityNameAttempt}`,
+    { mode: "cors" },
+  );
+  const cities = await response.json();
+
+  return cities;
+}
+
+function createSuggestionBox() {
+  const suggestionBox = document.createElement("div");
+  suggestionBox.id = "suggestion-box";
+  suggestionBox.style.width = `${cityForm.clientWidth}px`;
+
+  return suggestionBox;
+}
+
+function fillCityList(cityList, cities) {
+  cities.forEach((city) => {
+    const citySelector = document.createElement("li");
+    citySelector.classList.add("city-name");
+    if (city.country === "United States of America") {
+      citySelector.textContent = `${city.name}, ${city.region}`;
+    } else {
+      citySelector.textContent = `${city.name}, ${city.country}`;
+    }
+
+    citySelector.addEventListener("click", () => setCity(city.url));
+
+    cityList.appendChild(citySelector);
+  });
+}
+
+function setCity(cityUrl) {
+  setContent(cityUrl);
+  cityForm.value = "";
+  closeSuggestionBox();
+}
+
+function closeSuggestionBox() {
+  const suggestionBox = document.getElementById("suggestion-box");
+
+  if (suggestionBox !== null) {
+    isSuggestionBoxOpen = false;
+    document.getElementById("city-search-form").removeChild(suggestionBox);
+  }
+}
